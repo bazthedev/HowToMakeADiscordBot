c5c41a6542f03db39e223278e1f88949f0d210ea
Baz <bazthedev@gmail.com>
2021-10-30 12:15:58 +0100

    Update main.py

--- Setup/main.py
+++ Setup/main.py
@@ -1,13 +1,13 @@
 import discord
 from discord.ext import commands
-import os
+import os # import all required packages above
 from keep_alive import keep_alive # you only need this if you decide to host on replit
 
 client = commands.Bot(command_prefix=commands.when_mentioned_or("!"), description="This is my discord.py bot!", case_insensative=True)
 
 @client.event
 async def on_ready():
-  print("The bot is online")
+  print("The bot is online") # prints this message once your bot logs in to discord
 
 keep_alive() # you only need this if you decide to host on replit
 client.run(os.getenv("TOKEN"))
